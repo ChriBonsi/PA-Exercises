6936a6bb0d914337c6219e1127cdbd29531cff88
Christian Bonsignore <christian.bonsignore@studenti.unicam.it>
2023-01-24 16:34:50 +0100

    End of lesson (10)

--- BattleShip/app/src/main/java/it/unicam/cs/pa/battleship/Battlefield.java
+++ BattleShip/app/src/main/java/it/unicam/cs/pa/battleship/Battlefield.java
@@ -6,7 +6,7 @@ package it.unicam.cs.pa.battleship;
 public interface Battlefield {
 
     /**
-     * Adds a new ship on the battlefield starting from the coordinates towards the specified direction
+     * Adds a new ship on the battlefield starting from the coordinates towards the specified direction.
      * @param ship the ship
      * @param row the row of the coordinates
      * @param column the column of the coordinates
@@ -14,6 +14,18 @@ public interface Battlefield {
      * @return true if the ship is successfully added, false otherwise.
      */
     boolean addship(Ship ship, int row, int column, Direction dir);
+
+    /**
+     * Adds a new ship on the battlefield starting from the coordinates towards the default direction.
+     * @param ship the ship
+     * @param row the row of the coordinates
+     * @param column the column of the coordinates
+     * @return true if the ship is successfully added, false otherwise.
+     */
+    default boolean addship(Ship ship, int row, int column) {
+        return addship(ship, row, column, Direction.NORTH);
+    };
+
 
     /**
      * Shoot at the specified position.
@@ -30,5 +42,27 @@ public interface Battlefield {
      * @return the ship in the location, null otherwise.
      */
     Ship shipAt(int row, int column);
+
+    /**
+     * Checks if the specified location contains a ship.
+     * @param row the row of the location
+     * @param column the column of the location
+     * @return true if the location has no ship, false otherwise.
+     */
+    default boolean isFree(int row, int column){
+        return shipAt(row, column) == null;
+    };
+
+    /**
+     * @return the width of the battlefield.
+     */
+    int width();
+
+    /**
+     * @return the height of the battlefield.
+     */
+    int height();
+
+    ShotResult status(int row, int column);
 
 }
