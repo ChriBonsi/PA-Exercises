b03c60a6cadd3cc633936db1408a26a2460204be
Christian Bonsignore <christian.bonsignore@studenti.unicam.it>
2023-02-20 22:26:37 +0100

    End of lesson 16

--- Budget/api/src/main/java/it/unicam/cs/pa/budget/list/SimpleLedger.java
+++ Budget/api/src/main/java/it/unicam/cs/pa/budget/list/SimpleLedger.java
@@ -8,12 +8,20 @@ public class SimpleLedger implements Ledger{
     private final FactoryRegistry<Ledger,Transaction> transactions;
 
     public SimpleLedger() {
-        this.transactions = new FactoryRegistry<>(new BiFunction<Integer, Ledger, Transaction>() {
+        //Previous concept with hidden class
+        /*this.transactions = new FactoryRegistry<>(new BiFunction<Integer, Ledger, Transaction>() {
             @Override
             public Transaction apply(Integer integer, Ledger ledger) {
                 return new LedgerTransaction(integer,ledger);
             }
-        });
+        });*/
+
+        //Lambda expression
+        //this.transactions = new FactoryRegistry<>((id, ledger) -> new LedgerTransaction(id, ledger));
+
+        //Reference method
+        this.transactions = new FactoryRegistry<>(LedgerTransaction::new);
+
     }
 
     public Transaction newTransaction(){
